574e8ec53ae27a58f66179c8376ea4eb0b4ae712
caldenrodrigues <caldenrodrigues1202@gmail.com>
2019-01-29 19:11:04 +0530

    Created login.js in routes

---
+++ routes/login.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+// MySQL Connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.database_user,
+    password: process.env.database_password,
+    database: 'CsiManagementSystem'
+});
+
+connection.connect(function(err) {
+    if (!err) {
+        console.log('Connected to MySql!\n');
+    } else {
+        console.log(err);
+    }
+});
+
+router.post('/', (req, res) =>{
+     var id= req.body.id;
+     var password = req.body.password;
+     //Query to select the tuple of the user
+     connection.query('SELECT * FROM users WHERE id = ?',[id], function (error, results, fields) {
+     if (error) {
+      console.log(error);
+      res.status(404);
+     }else{
+       if(results.length >0){
+          //User exists
+          if(results[0].password == password){
+             //Users password match
+             res.status(200).send({
+               "role":results[0].role
+             });
+          }
+	        else{
+            //Users password do not match
+            res.sendStatus(404);
+          }
+       }
+       else{
+         //User does not exist
+         res.sendStatus(404);
+       }
+      }
+    });
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,66 +1,15 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
+
+var login = require('./routes/login');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// MySQL Connection
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.database_user,
-    password: process.env.database_password,
-    database: 'CsiManagementSystem'
-});
-
-connection.connect(function(err) {
-    if (!err) {
-        console.log('Connected to MySql!\n');
-    } else {
-        console.log(err);
-    }
-});
-
-//server scripting
-var router=express.Router();
-app.post('/login', (req, res) =>{
-     var id= req.body.id;
-     var password = req.body.password;
-     //console.log(id,password);
-     connection.query('SELECT * FROM users WHERE id = ?',[id], function (error, results, fields) {
-     if (error) {
-      //console.log("error ocurred",error);
-      res.status(400).send({
-      "failed":"error ocurred"
-      })
-     }else{
-        // console.log('The solution is: ', results);
-       if(results.length >0){
-          if(results[0].password == password){
-             res.status(200).send(
-             JSON.parse(JSON.stringify(`{'role':'" ${results[0]}.role "'}`))
-             {
-               "role":results[0].role
-             }
-             );
-          }
-	else{
-          res.status(204).send({
-          "success":"Email and password does not match"
-          });
-        }
-       }
-       else{
-         res.status(204).send({
-         "success":"Email does not exits"
-         });
-       }
-      }
-    });
-});
+app.use('/login',login);
 
 //port activation
 app.listen(8080, (req, res) => {
