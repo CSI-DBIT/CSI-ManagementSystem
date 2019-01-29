7b9c24de97b377d16aa530bcfab11a01f6bd2a2e
caldenrodrigues <caldenrodrigues1202@gmail.com>
2019-01-29 14:00:28 +0530

    modified server.js and created Android

--- server.js
+++ server.js
@@ -1,44 +1,18 @@
-/*const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-
-var port = process.env.PORT || 8080;
-
-// Body-parser Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.get("/test",(req,res)=>{
-	return res.send("Welcome to CSI DBIT!!!");
-});
-
-// Server start
-app.listen(port, (req, res) => {
-    console.log("Listening to port");
-});                                    */
-
-
-
-
-
-//BACKEND-LOGIN
-
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 // MySQL Connection
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'oracle',
+    user: process.env.database_user,
+    password: process.env.database_password,
     database: 'CsiManagementSystem'
 });
 
@@ -46,41 +20,41 @@ connection.connect(function(err) {
     if (!err) {
         console.log('Connected to MySql!\n');
     } else {
-        console.log('Not connected to MySql.\n');
+        console.log(err);
     }
 });
 
 //server scripting
 var router=express.Router();
-router.post('/login', (req, res) =>{
+app.post('/login', (req, res) =>{
      var id= req.body.id;
      var password = req.body.password;
+     //console.log(id,password);
      connection.query('SELECT * FROM users WHERE id = ?',[id], function (error, results, fields) {
      if (error) {
       //console.log("error ocurred",error);
-      res.send({
-      "code":400,
+      res.status(400).send({
       "failed":"error ocurred"
       })
      }else{
         // console.log('The solution is: ', results);
        if(results.length >0){
           if(results[0].password == password){
-             res.send({
-             "code":200,
-             "success":"login sucessfull"
-             });
+             res.status(200).send(
+             JSON.parse(JSON.stringify(`{'role':'" ${results[0]}.role "'}`))
+             {
+               "role":results[0].role
+             }
+             );
           }
 	else{
-          res.send({
-          "code":204,
+          res.status(204).send({
           "success":"Email and password does not match"
           });
         }
        }
        else{
-         res.send({
-         "code":204,
+         res.status(204).send({
          "success":"Email does not exits"
          });
        }
@@ -92,6 +66,3 @@ router.post('/login', (req, res) =>{
 app.listen(8080, (req, res) => {
     console.log("Listening on 8080");
 });
-
-
- 
