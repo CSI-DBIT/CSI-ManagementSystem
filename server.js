88e97aa8843494e9656b0f9cef314a1ba5fad4be
survir44 <survir44@gmail.com>
2019-03-06 02:25:31 +0530

    Server Updated

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 
 var login = require('./routes/login');
 var minutes = require('./routes/minutes');
+var profile = require('./routes/profile');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({
 
 app.use('/login',login);
 app.use('/minutes',minutes);
+app.use('/profile',profile);
 
 app.get("/", (req,res)=>{
 return res.send("Welcome to CSI-DBIT");
