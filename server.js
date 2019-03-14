1b5be97ebd89d129563b0f1c43cb47aade018129
Calden Rodrigues <caldenrodrigues1202@gmail.com>
2019-03-14 21:35:43 +0530

    Fixed a lot of bugs

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(cors());
 var login = require('./routes/login');
 var minutes = require('./routes/minutes');
 var profile = require('./routes/profile');
+var attendance = require('./routes/attendance');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({
 app.use('/login',login);
 app.use('/minutes',minutes);
 app.use('/profile',profile);
+app.use('/attendance',attendance);
 
 app.get("/", (req,res)=>{
 return res.send("Welcome to CSI-DBIT");
