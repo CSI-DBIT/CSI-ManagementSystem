d8cb4704bfeab3862778913c6e9329b814321ccf
Mushira Shaikh <46518140+mushi99@users.noreply.github.com>
2019-03-21 16:51:13 +0530

    updated attendance.js

--- routes/attendance.js
+++ routes/attendance.js
@@ -1,13 +1,13 @@
 var express = require('express');
-var router = express();
-var bodyParser = require('body-parser');
+var router = express.Router();
 var mysql = require('mysql');
 
 // MySQL Connection
+var mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'pmauser',//process.env.database_user,
-    password: 'Mushira_99',//process.env.database_password,
+    user: process.env.database_user,
+    password: process.env.database_password,
     database: 'CsiManagementSystem'
 });
 
@@ -19,14 +19,9 @@ connection.connect(function(err) {
     }
 });
 
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({
-    extended: true
-}));
 
 
-
-router.post('/attendance/request',(req,res)=>{
+router.post('/request',(req,res)=>{
 	var id = req.body.id;
 	var date = req.body.date;
 	var s1 = req.body.s1;
@@ -36,7 +31,7 @@ router.post('/attendance/request',(req,res)=>{
 	var s5 = req.body.s5;
 	var s6 = req.body.s6;
 	var s7 = req.body.s7;
-	var reason = req.body.reason	
+	var reason = req.body.reason
 	console.log(id);
 	//fetching name from users table
 	connection.query('SELECT name FROM profile WHERE profile.id=?',[id],function(error,name,fields){
@@ -45,64 +40,67 @@ router.post('/attendance/request',(req,res)=>{
 		res.sendStatus(400);
 	}
 	else{
-		//pushing into request table 
+		//pushing into request table
 		connection.query('INSERT INTO request VALUES(?,?,?,?,?,?,?,?,?,?,?,?)',[Math.random().toString(36).replace('0.',''),id,name[0].name,date,s1,s2,s3,s4,s5,s6,s7,reason],function(err,results,fields){
 		if (err){
-			console.log(err);			
+			console.log(err);
 			res.sendStatus(400);
 		}
 		else{
+			console.log("Data Inserted");
 				res.sendStatus(200);
-				console.log("Data Inserted");
+				
 			}
 		});
 	}
 	});
 });
- 
+
 //display all the requests:
-router.post('/attendance/requestlist', (req, res) =>{
-	connection.query('SELECT* FROM request', function (error, results, fields) {
+router.post('/requestlist', (req, res) =>{
+	connection.query('SELECT * FROM request', function (error, results, fields) {
 	if (error){
-		console.log(error)
+		//console.log(error)
 		res.sendStatus(400);
 	}
 	else
 	{
-		res.sendStatus(200);
-		res.send(results);
+    res.status(400).json(results);
+    //res.send(results).sendStatus(200);
 	}
 	});
 });
 
 //accept json array, move the record from request to final_list
-router.post('/attendance/finallist', (req, res) =>{
-	connection.query('INSERT INTO final_list SELECT RID, stud_id, Name, date, s1, s2, s3, s4, s5, s6, s7 FROM request WHERE RID = ?)',[req.body.accepted[0]], function (error, fields){
+router.post('/finallist', (req, res) =>{
+  console.log(req.body.accepted[0])
+  for (var i = 0; i < req.body.accepted.length; i++)
+{
+	var rid = req.body.accepted[i]
+	connection.query('INSERT INTO final_list SELECT RID, stud_id, name, date, s1, s2, s3, s4, s5, s6, s7 FROM request WHERE RID = ?',[rid], function (error, fields){
 	if (error){
 		console.log(error)
 		res.sendStatus(400);
 	}
 	else
 	{
-		res.sendStatus(200);
-		// display the accepted requests
-		connection.query('SELECT* FROM request', function (error, results, fields) {
-			if (error){
-				console.log(error)
-				res.sendStatus(400);
-			}	
-			else
-			{
-				res.sendStatus(200);
-				res.send(results);
-			}
+		connection.query('delete from request where RID = ?',[rid], function (error, results, fields) {
+		if (error){
+			console.log(error)
+			res.sendStatus(400);
+		}
+		else
+		{
+			console.log("Deleted succesfully");
+	    	//res.sendStatus(200);
+		}
 		});
+		console.log("Inserted succesfully");
+		res.sendStatus(200);
+		
 	}
 	});
+}
 });
 
-
-router.listen(8080, (req, res) => {
-    console.log("Listening on 8080");
-});
-//module.exports = router;
+module.exports = router;
