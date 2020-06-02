a32c06290f531ad5c1f6970d8889dbb1c3cc51c7
survir44 <survir44@gmail.com>
2020-06-02 18:28:20 +0530

    Feedback API

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,10 +1,34 @@
 var express=require('express');
 var router=express.Router();
 
-//Feedback 
+//Email
+var nodemailer=require('nodemailer');
+var transporter=nodemailer.createTransport({
+	service:'gmail',
+	auth:{
+		user:'generixteam2019@gmail.com',
+		pass:'Lifeisgud'
+	}
+});
+
+//Request Handler
 router.post('/',(req,res)=>{
-			res.status(200).send({"message":"Feedback Mail is in progress"});
-	
+	var mailOptions={
+		from:'generixteam2019@gmail.com',
+		to:'generixteam2019@gmail.com',
+		subject:'Feedback',
+		text:'UserID:'+ req.body.id+'\nName:'+req.body.name+'\nFeedback:'+req.body.feedback
+	};
+	transporter.sendMail(mailOptions,function(error,info){
+		if(error){
+			console.log("Mail Error");
+			res.sendStatus(400);
+		}
+		else{
+		console.log('Email sent:'+ info.response);
+		res.sendStatus(200);
+		}
+	});
 });
 
 module.exports=router;
