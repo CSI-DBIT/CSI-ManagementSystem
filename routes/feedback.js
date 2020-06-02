177bcc0a660b4b21c6c8bd75be4819efc648c802
survir44 <survir44@gmail.com>
2020-06-02 17:59:23 +0530

    Feedback testing

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,8 +1,8 @@
 var express=require('express');
 var router=express.Router();
 
-
-router.post('/feedback',(req,res)=>{
+//Feedback 
+router.post('/',(req,res)=>{
 			res.status(200).send({"message":"Feedback Mail is in progress"});
 	
 });
