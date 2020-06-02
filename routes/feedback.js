e7ff2815fad82caf2e0cdda3234430126e13dc02
survir44 <survir44@gmail.com>
2020-06-02 20:27:10 +0530

    Feedback API

--- routes/feedback.js
+++ routes/feedback.js
@@ -6,16 +6,16 @@ var nodemailer=require('nodemailer');
 var transporter=nodemailer.createTransport({
 	service:'gmail',
 	auth:{
-		user:process.env.email_id,
-		pass: process.env.email_password
+		user:'generixteam2019@gmail.com',
+		pass: 'Lifeisgud'
 	}
 });
 
 //Request Handler
 router.post('/',(req,res)=>{
 	var mailOptions={
-		from:process.env.email_id,
-		to:process.env.email_id,
+		from:'generixteam2019@gmail.com',
+		to:'generixteam2019@gmail.com',
 		subject:'CSI-App Feedback',
         html: '<p><span style="font-size: 18px;">Greetings!</span></p><br><p>Here is the feedback provided by <strong>"'+req.body.name+'"</strong> with id <strong>"'+req.body.id+'"</strong>:</p><br>'+req.body.feedback+'<br><br><br>Regards,<br><strong>CSI-Management APP development team.</strong>'
 	};
