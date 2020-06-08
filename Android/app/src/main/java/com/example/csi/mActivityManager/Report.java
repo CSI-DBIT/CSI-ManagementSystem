91703a45c83e741138c42b1b238db6423ef400dc
AFIF <46858532+Afif79@users.noreply.github.com>
2020-06-08 17:11:54 +0530

    Day 6 changes Done

--- Android/app/src/main/java/com/example/csi/mActivityManager/Report.java
+++ Android/app/src/main/java/com/example/csi/mActivityManager/Report.java
@@ -2,6 +2,7 @@ package com.example.csi.mActivityManager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.csi.R;
 
@@ -11,5 +12,17 @@ public class Report extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+        getSupportActionBar().setTitle("Reports");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        // TODO Auto-generated method sub
+        int id= item.getItemId();
+        if (id == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
