21b52b3e6882614b8dc2990b4cdd0cee38c368cb
AFIF <46858532+Afif79@users.noreply.github.com>
2020-06-11 17:36:25 +0530

    depth animation to image slider

--- Android/app/src/main/java/com/example/csi/mFragments/ActivityManager.java
+++ Android/app/src/main/java/com/example/csi/mFragments/ActivityManager.java
@@ -77,40 +77,36 @@ public class ActivityManager extends Fragment  {
     }
    // image slider animation
     public static class DepthPageTransformer implements ViewPager.PageTransformer {
-        private static final float MIN_SCALE = 0.75f;
+       @Override
+       public void transformPage(View page, float position) {
 
-        public void transformPage(View view, float position) {
-            int pageWidth = view.getWidth();
+           if (position < -1){    // [-Infinity,-1)
+               // This page is way off-screen to the left.
+               page.setAlpha(0);
 
-            if (position < -1) { // [-Infinity,-1)
-                // This page is way off-screen to the left.
-                view.setAlpha(0f);
+           }
+           else if (position <= 0){    // [-1,0]
+               page.setAlpha(1);
+               page.setTranslationX(0);
+               page.setScaleX(1);
+               page.setScaleY(1);
 
-            } else if (position <= 0) { // [-1,0]
-                // Use the default slide transition when moving to the left page
-                view.setAlpha(1f);
-                view.setTranslationX(0f);
-                view.setScaleX(1f);
-                view.setScaleY(1f);
+           }
+           else if (position <= 1){    // (0,1]
+               page.setTranslationX(-position*page.getWidth());
+               page.setAlpha(1-Math.abs(position));
+               page.setScaleX(1-Math.abs(position));
+               page.setScaleY(1-Math.abs(position));
 
-            } else if (position <= 1) { // (0,1]
-                // Fade the page out.
-                view.setAlpha(1 - position);
+           }
+           else {    // (1,+Infinity]
+               // This page is way off-screen to the right.
+               page.setAlpha(0);
 
-                // Counteract the default slide transition
-                view.setTranslationX(pageWidth * -position);
+           }
 
-                // Scale the page down (between MIN_SCALE and 1)
-                float scaleFactor = MIN_SCALE
-                        + (1 - MIN_SCALE) * (1 - Math.abs(position));
-                view.setScaleX(scaleFactor);
-                view.setScaleY(scaleFactor);
 
-            } else { // (1,+Infinity]
-                // This page is way off-screen to the right.
-                view.setAlpha(0f);
-            }
-        }
+       }
     }
 // animation ends here
 
